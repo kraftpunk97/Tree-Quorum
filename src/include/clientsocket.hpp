ef1c791f32011c9e46c807abe1473abccc23ba8a
Kartikey Gupta <kxg220013@dc01.utdallas.edu>
2023-03-28 23:00:06 -0500

    Added is_valid check

--- src/include/clientsocket.hpp
+++ src/include/clientsocket.hpp
@@ -16,6 +16,7 @@ public:
     void recv(void* buffer, size_t len);
     void send(const void* buffer, size_t len);
     void connect(const std::string host, const int port);
+    bool is_valid() {return Socket::is_valid(); };
     void close();
 
     void set_non_blocking(const bool blocking) { Socket::set_non_blocking(blocking); };
